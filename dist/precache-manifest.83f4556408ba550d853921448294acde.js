ce5490f7fe0e5a0c7e86ae5f793c64c4618f8e10
David Squire <squiredd@gmail.com>
2018-12-20 20:31:20 -0800

    added dist folder

---
+++ dist/precache-manifest.83f4556408ba550d853921448294acde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1281eb0365e75c07e9ad",
+    "url": "/static/js/main.1281eb03.chunk.js"
+  },
+  {
+    "revision": "bc29fee67ede6899c699",
+    "url": "/static/js/1.bc29fee6.chunk.js"
+  },
+  {
+    "revision": "1281eb0365e75c07e9ad",
+    "url": "/static/css/main.6041de4a.chunk.css"
+  },
+  {
+    "revision": "6a7153eafd96f9c99caef7d9ce7d4733",
+    "url": "/index.html"
+  }
+];
